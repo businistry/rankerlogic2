96dc666944ca9cde607cabef465bf2429aead544
Todd Curry <tcurryjr4@gmail.com>
2025-02-01 21:31:59 -0600

    dffin

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/[your-repo-name]/', // Match your GitHub repository name
+  base: '/rankerlogic2/', // Match your GitHub repository name
   build: {
     outDir: 'dist',
     emptyOutDir: true
